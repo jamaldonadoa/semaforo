d5290deda45c6fda894b8ed87f313efc57e4a5b0
jamaldonadoa <jadolfomaldonadoa@gmail.com>
2021-10-23 22:11:21 +0000

    Modificaciones hechas

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,59 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 //create your first component
-const Home = () => {
+export const Home = () => {
+	const [valueRojo, setValueRojo] = useState("rojo");
+	const [valueAmarillo, setValueAmarillo] = useState("amarillo");
+	const [valueVerde, setValueVerde] = useState("verde");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="tubo"></div>
+			<div className="semaforo d-flex flex-column justify-content-around">
+				<div
+					className={valueRojo}
+					onClick={e => {
+						if (valueRojo == "rojo iluminado") {
+							setValueRojo("rojo");
+							setValueAmarillo("amarillo");
+							setValueVerde("verde");
+						} else {
+							e.className = setValueRojo("rojo iluminado");
+							setValueAmarillo("amarillo");
+							setValueVerde("verde");
+						}
+					}}></div>
+				<div
+					className={valueAmarillo}
+					onClick={e => {
+						if (valueAmarillo == "amarillo iluminado") {
+							setValueRojo("rojo");
+							setValueAmarillo("amarillo");
+							setValueVerde("verde");
+						} else {
+							e.className = setValueAmarillo(
+								"amarillo iluminado"
+							);
+							setValueRojo("rojo");
+							setValueVerde("verde");
+						}
+					}}></div>
+				<div
+					className={valueVerde}
+					onClick={e => {
+						if (valueVerde == "verde iluminado") {
+							setValueRojo("rojo");
+							setValueAmarillo("amarillo");
+							setValueVerde("verde");
+						} else {
+							e.className = setValueVerde("verde iluminado");
+							setValueAmarillo("amarillo");
+							setValueRojo("rojo");
+						}
+					}}></div>
+			</div>
+		</>
 	);
 };
 
